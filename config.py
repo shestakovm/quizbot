0c00f749996bd3d3ec59940ebe18294554887fcf
Matvei Shestakov <107620240+shestakovm@users.noreply.github.com>
2025-06-25 20:54:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "8099810498:AAGn8OZKttwlAjHAsKGsMHHQsh6H7aY41X4"
-ADMIN_IDS = [54465054, 395208926]  # Список ID администраторов
+BOT_TOKEN = "bot token"
+ADMIN_IDS = [123123]  # Список ID администраторов
 
 # Настройки временных зон и форматов
 TIMEZONE = 'Europe/Moscow'
